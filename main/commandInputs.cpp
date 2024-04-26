a219f39d52cfc415c505a78c85a35e14c3d62aff
SpiroGarcia <111991438+SpiroGarcia@users.noreply.github.com>
2024-04-25 20:06:36 -0400

    added error checking in command input

--- main/commandInputs.cpp
+++ main/commandInputs.cpp
@@ -21,22 +21,61 @@ for (JsonPair member : doc.as<JsonObject>()) {
   const char* currentKey = member.key().c_str();
   //command verification
     if (strcmp(currentKey,"command") == 0 && member.value().is<String>()) commandType = member.value();
-    else if (strcmp(currentKey,"command") == 0 && !(member.value().is<String>())) Serial.println("Invalid Input");
+    else if (strcmp(currentKey,"command") == 0 && !(member.value().is<String>())) {
+      Serial.println("Invalid Input");
+      break;
+    }
   //Temperature 
-    else if((strcmp(currentKey, "indoor") == 0) && strcmp(commandType,"temperature") == 0) Vals.tempIndoor = member.value().as<float>();
-    else if((strcmp(currentKey, "outdoor") == 0) && strcmp(commandType,"temperature") == 0) Vals.tempOutdoor = member.value().as<float>();
+    else if (strcmp(commandType,"temperature") == 0) {
+      if(strcmp(currentKey, "indoor") == 0 && member.value().is<float>()) Vals.tempIndoor = member.value();
+      else if(strcmp(currentKey, "outdoor") == 0 && member.value().is<float>()) Vals.tempOutdoor = member.value();
+      else {
+        Serial.println("Invalid Input");
+        break;
+      }
+    }
+
   //Humidity
-    else if((strcmp(currentKey, "indoor") == 0) && strcmp(commandType,"humidity") == 0) Vals.humIndoor = member.value().as<float>();
-    else if((strcmp(currentKey,"outdoor") == 0) && strcmp(commandType,"humidity") == 0) Vals.humOutdoor = member.value().as<float>();
+    else if (strcmp(commandType,"humidity") == 0) {
+      if(strcmp(currentKey, "indoor") == 0 && member.value().is<float>()) Vals.humIndoor = member.value();
+      else if(strcmp(currentKey, "outdoor") == 0 && member.value().is<float>()) Vals.humOutdoor = member.value();
+      else {
+        Serial.println("Invalid Input");
+        break;
+      }
+    }
+  
   //Wind
-    else if((strcmp(currentKey, "speed") == 0) && strcmp(commandType,"wind") == 0) Vals.windSpeed = member.value().as<float>();
-    else if((strcmp(currentKey,"direction") == 0) && strcmp(commandType,"wind") == 0) Vals.windDir = member.value().as<float>();
+    else if(strcmp(commandType,"wind") == 0) {
+      if(strcmp(currentKey, "speed") == 0 && member.value().is<float>()) Vals.windSpeed = member.value();
+      else if(strcmp(currentKey,"direction") == 0 && member.value().is<float>()) Vals.windDir = member.value();
+      else {
+        Serial.println("Invalid Input");
+        break;
+      }
+    }
   //Rain
-    else if((strcmp(currentKey, "pwm") == 0) && strcmp(commandType,"wind") == 0) Vals.rainPwm = member.value().as<float>();
+    else if(strcmp(commandType,"rain") == 0) {
+      if (strcmp(currentKey, "pwm") == 0 && member.value().is<float>()) Vals.rainPwm = member.value();
+      else {
+        Serial.println("Invalid Input");
+        break;
+      }
+    }
   //Moisture
-    else if((strcmp(currentKey,"out1") == 0) && strcmp(commandType,"moisture") == 0) Vals.windSpeed = member.value().as<float>();  
-    else if((strcmp(currentKey,"out2") == 0) && strcmp(commandType,"moisture") == 0) Vals.windDir = member.value().as<float>();
-    else if((strcmp(currentKey,"out3") == 0) && strcmp(commandType,"moisture") == 0) Vals.windDir = member.value().as<float>();
+    else if(strcmp(commandType,"moisture") == 0) {
+      if (strcmp(currentKey, "out1") == 0 && member.value().is<float>()) Vals.out1 = member.value();
+      else if (strcmp(currentKey, "out2") == 0 && member.value().is<float>()) Vals.out2 = member.value();
+      else if (strcmp(currentKey, "out3") == 0 && member.value().is<float>()) Vals.out3 = member.value();
+      else {
+        Serial.println("Invalid Input");
+        break;
+      }
+    }
+    else {
+        Serial.println("Invalid Input");
+        break;
+    }
 
     
 }
