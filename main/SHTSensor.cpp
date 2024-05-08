25df363ccb05be9c76cfbabdf13136d5ec597ae9
CassySch <cassandra.schneider.benard@gmail.com>
2024-05-08 00:47:50 -0400

    Changed SHT sensor FSM to run in ISR
    The reading and writing is based on the clock so the fsm should be in an ISR that is triggred on the clock changes.

--- main/SHTSensor.cpp
+++ main/SHTSensor.cpp
@@ -3,7 +3,7 @@
 
 //#define  TWELVE_BIT_HUM
 #define EIGHT_BIT_HUM
-/*
+
 #ifdef TWELVE_BIT_HUM
   #define C1 -2.0468
   #define C2 0.0367
@@ -18,60 +18,65 @@
 
 #define SDA_PIN
 #define SCL_PIN
-bool clockPulse[15];
-bool dataPulse[15];
+
+#define READ 0
+#define WRITE 1
+
+
+bool actionType = READ;
+float soRh;
 
 void initSht(){
   pinMode(SDA_PIN,INPUT);
   pinMode(SCL_PIN,INPUT);
-  attachInterrupt(digitalPinToInterrupt(SCL_PIN), clockVals, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(SDA_PIN), dataVals, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(SCL_PIN), readWrite, CHANGE);
 }
-void IRAM_ATTR dataVals(){
-  clockPulse[i++] = digitalRead(SDA_PIN);
+
+
+
+void IRAM_ATTR readWrite(){
+  enum state = {idle = 0,read,sendAck,writeHum,writeTemp,receiveAck} state = idle;
+  if(digitalRead(SCL_PIN)== 0) lowCnt++;
+  switch(state){
+    case idle:
+        if !(digitalRead(SDA_PIN)&&(digitalRead(SCL_PIN)) state = read;
+        else state = idle;
+      break;
+    case read:
+        
+      break;
+    case sendAck:
+      if(lowCnt == 8) digitalWrite(SDA_PIN,LOW);
+      else if(lowCnt == 9) digitalWrite(SDA_PIN,HIGH);
+      
+      if(command = readHum) state = writeHum;
+      else if (command = readTemp) state = writeTemp;
+      break;
+    case writeHum:
+    static float humidity = soRh;
+    for (int i = sizeof(humidity) * 8 - 1; i >= 0; i--) {
+          // Extract the i-th bit from num
+          int bit = (humidity >> i) & 1;
+          digitalWrite(struct.sdaPin,bit);
+      break;
+    case writeTemp:
+      break;
+    case receiveAck:
+      break;
+  }
+  if(actionType){
+    attachInterrupt(digitalPinToInterrupt(SCL_PIN), readWrite, FALLING);
+    }
 }
-void IRAM_ATTR clockVals(){
-  dataPulse[i++] = digitalRead(SCL_PIN);
-}
+
+
 
 float convertToSoRh(float rhLin){
-  float soRh, rhLin;
+  float rhLin;
   soRh = (-(C2)+sqrt(pow(C2,2)-4(C3*(C1-rhLin))))/(2*C3);
-
-  return soRh;
 }
 
-void sendHumidity(float soRh){
-  digitalWrite(SDA_PIN,soRh);
-}
 
-void sendTemperature(float soT){
+void convertToSoT(float soT){
+  //Have to figure out the conversion
 }
-
-enum state = {idle = 0,recieving,writing,reading} state = idle;
-//enum dataSequenceState = {dataLow = 0,dataHigh};
-//enum clockSequenceState = {clockLow = 0,clockHigh};
-bool startSequence[][] ={1,0,1},{0,0,1};
-bool correct = 0;
-bool command[];
-switch(state){
-  case idle:
-      for(int j =0; j<3; j++){
-        if (startSequence[0][j]) == clockPulse[j]) && (startSequence[1][j]) == dataPulse[j]) correct = 1;
-        else {
-          correct = 0;
-          break;
-          }
-      }
-      if (correct == 1) state = recieving;
-      else state = idle;
-    break;
-  case recieving:
-
-    break;
-  case writing:
-    break;
-  case reading:
-    break;
-}
-*/
