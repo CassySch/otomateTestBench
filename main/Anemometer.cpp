9d77d5be9840b9c38d42bb87bfc46c572ece6753
SpiroGarcia <111991438+SpiroGarcia@users.noreply.github.com>
2024-05-09 16:48:24 -0400

    Update Anemometer.cpp

--- main/Anemometer.cpp
+++ main/Anemometer.cpp
@@ -7,6 +7,6 @@ void SetWind(float speed, int degrees){
   pulse_ms = (1000/(speed/2.4)) - 15; // calculates the duration of one pwm pulse, (half the period)
   setPwm(pulse_ms, pulse_ms, PinAnemometer); //sets pwm pulse signal according to wind speed set
   
-  SensorOut = ((degrees)/360)*1023)/(5.0/1023.0); //determines output voltage for simulated anemometer
+  SensorOut = ((degrees/360)*1023)/(5.0/1023.0); //determines output voltage for simulated anemometer
   SendToDac(DAC_1_ADDRESS, SensorOut, GROUP_ANEMOMETER); //sets the DAC to the analog voltage value corresponding to the wind direction
 }
